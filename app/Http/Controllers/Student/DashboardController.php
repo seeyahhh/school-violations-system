0861f923042245d375213ca4375ad9e5654ef87c
Xenon <joshua123.jdr@gmail.com>
2026-01-03 00:01:13 +0800

    feat: add data fetching for student dashboard

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        $user = Auth::user();
+        $violationRecord = $user->violationRecord()->latest()->get();
+        $violation_count = $violationRecord->count();
+
+        $data = [
+            'user' => $user,
+            'violation_count' => $violation_count,
+            'violation_record' => $violationRecord,
+        ];
+
+        // compact data to frontend soon
         return view('student.dashboard');
     }
 }
