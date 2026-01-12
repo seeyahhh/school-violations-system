a3bcac5c0ff0b1c740372c3be6e5d10185d177e2
seeyahhh <aysemcatampatan@gmail.com>
2026-01-12 19:25:52 +0800

    refactor: add logo

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin Dashboard</title>
+    <link rel="icon" type="image/png" href="{{ asset('PUPLogo 1 Login.png') }}">
+
     @vite(entrypoints: ['resources/sass/app.scss', 'resources/js/app.js'])
 
 </head>
--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Portal | Violation Dashboard</title>
+    <link rel="icon" type="image/png" href="{{ asset('PUPLogo 1 Login.png') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
